a68296ada7af2259143bca5f48603d26a6c239d9
Mark Stan <stanmarksebastian@gmail.com>
2020-11-17 20:01:45 +0200

    added password config with BCrypt

--- src/main/java/com/codecool/flight_api_project/security/AppSecurityConfig.java
+++ src/main/java/com/codecool/flight_api_project/security/AppSecurityConfig.java
@@ -1,18 +1,29 @@
 package com.codecool.flight_api_project.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    AppSecurityConfig(){
+    private final PasswordEncoder passwordEncoder;
 
+
+    @Autowired
+    public AppSecurityConfig(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
     }
 
 
@@ -26,4 +37,19 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated();
     }
+
+    protected UserDetailsService userDetailsService(){
+        UserDetails sorin = User.builder()
+                .username("sorin")
+                .password(passwordEncoder.encode("password"))
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                sorin
+        );
+    }
+
+
+
+
 }
