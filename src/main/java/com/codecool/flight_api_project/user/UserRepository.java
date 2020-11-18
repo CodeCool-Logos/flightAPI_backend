b2660b23a11e9174f6802709bafc1aa27c895a2f
sorinobreja <sorinobreja93@gmail.com>
2020-11-18 17:03:51 +0200

    created user package

--- src/main/java/com/codecool/flight_api_project/user/UserRepository.java
+++ src/main/java/com/codecool/flight_api_project/user/UserRepository.java
@@ -1,35 +1,11 @@
 package com.codecool.flight_api_project.user;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-
-
-    private static List<User> DB = new ArrayList<>();
-
-    public void populatedUsersList() throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        DB = objectMapper.readValue(
-                new File("src/main/resources/data.json"),
-                new TypeReference<List<User>>(){});
-    }
-
-    public void insertUser(User user){
-        DB.add(user);
-    }
-
-
-    public List<User> findAll(){
-        return DB;
-    }
 }
