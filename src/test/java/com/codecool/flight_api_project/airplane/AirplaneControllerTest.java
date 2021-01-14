78f6132fedf1e44ddcac44e135fb4fa7b2cfb15c
Alexandru Oriean <alexandru.oriean@gmail.com>
2021-01-14 13:00:06 +0200

    Added test package for airplane

---
+++ src/test/java/com/codecool/flight_api_project/airplane/AirplaneControllerTest.java
@@ -0,0 +1,32 @@
+package com.codecool.flight_api_project.airplane;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AirplaneControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAirplanes() {
+    }
+
+    @Test
+    void getAirplaneBy() {
+    }
+
+    @Test
+    void addAirplane() {
+    }
+
+    @Test
+    void deleteAirplaneById() {
+    }
+
+    @Test
+    void updateAirplaneById() {
+    }
+}
